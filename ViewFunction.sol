7e47a792f7a715e37b9023b949b97a65628f32c0
Bhavesh Chandani <77831291+bhavesh555@users.noreply.github.com>
2023-02-27 12:55:24 +0530

    Create View and Pure Function

---
+++ ViewFunction.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+contract ExampleViewPure {
+
+    uint public myStorageVariable;
+
+    function getMyStorageVariable() public view returns(uint) {
+        return myStorageVariable;
+    }
+
+    function setMyStorageVariable(uint _newVar) public returns(uint) {
+        myStorageVariable = _newVar;
+        return _newVar;
+    }
+
+
+}
