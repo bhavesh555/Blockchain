64272960ccf27c1edfd3fda3fb8c4ad2b35e41b7
Bhavesh Chandani <77831291+bhavesh555@users.noreply.github.com>
2023-02-27 12:30:26 +0530

    Creating a writing function

---
+++ WritingFunction.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+contract ExampleViewPure {
+
+    uint public myStorageVariable;
+
+    function setMyStorageVariable(uint _newVar) public returns(uint) {
+        myStorageVariable = _newVar;
+        return _newVar;
+    }
+
+
+}
